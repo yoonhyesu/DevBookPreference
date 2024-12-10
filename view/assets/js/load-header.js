5cb4aa1b8990f380de0a61a049493b8b0d4ef37c
yoonhyesu <everybodya@naver.com>
2024-12-10 09:52:01 +0900

    전체 초기화

--- view/assets/js/load-header.js
+++ view/assets/js/load-header.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('../view/shared/header.html')
+    fetch('../shared/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
